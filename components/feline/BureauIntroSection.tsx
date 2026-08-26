131b29f78b2a3b5521a9b0d180ef98e0ccb40654
houchenran <1614779767@qq.com>
2026-08-26 11:37:30 +0800

    更换视频

--- components/feline/BureauIntroSection.tsx
+++ components/feline/BureauIntroSection.tsx
@@ -1220,7 +1220,7 @@ export default function BureauIntroSection({
             >
               <video
                 ref={videoRef}
-                src="https://my-video-bucket-1458721399.cos.ap-nanjing.myqcloud.com/videos/baiwan-25s-audio.mp4"
+                src="https://my-video-bucket-1458721399.cos.ap-nanjing.myqcloud.com/videos/v1-universal.mp4"
                 playsInline
                 autoPlay={!needsTapToPlay}
                 preload="auto"
